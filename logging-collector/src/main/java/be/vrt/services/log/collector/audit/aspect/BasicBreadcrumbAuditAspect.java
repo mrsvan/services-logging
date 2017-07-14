bb37a07d476b572511894ea32126b8e27b0ec387
Stefaan Vanderheyden <stefaan.vanderheyden@nuuvo.mobi>
2017-07-14 12:23:55 +0200

    SVC-5275 Create a "@AuditLogLevel" annotation (Improve annotation names)

--- logging-collector/src/main/java/be/vrt/services/log/collector/audit/aspect/BasicBreadcrumbAuditAspect.java
+++ logging-collector/src/main/java/be/vrt/services/log/collector/audit/aspect/BasicBreadcrumbAuditAspect.java
@@ -3,27 +3,24 @@ package be.vrt.services.log.collector.audit.aspect;
 import be.vrt.services.logging.api.audit.annotation.Level;
 import be.vrt.services.logging.log.common.LogTransaction;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.Consumer;
 
 public class BasicBreadcrumbAuditAspect extends AbstractBreadcrumbAuditAspect {
     @Override
     protected Object handleJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
-        log(joinPoint, l -> l.debug("[BREADCRUMB] " + joinPoint.getSignature().toShortString()));
+        log(joinPoint, "[BREADCRUMB] {}", joinPoint.getSignature().toShortString());
         try {
             return joinPoint.proceed();
         } catch (Exception e) {
-            log(joinPoint, l -> l.debug(
-                    "[BREADCRUMB-EXCEPTION] " + joinPoint.getSignature().toShortString() + " -> " + e.getMessage(), e));
+            log(joinPoint, "[BREADCRUMB-EXCEPTION] {} -> {}", joinPoint.getSignature().toShortString(), e.getMessage(),
+                    e);
             throw e;
         }
     }
 
-    private void log(ProceedingJoinPoint jointPoint, Consumer<Logger> consumer) {
+    private void log(ProceedingJoinPoint jointPoint, String msg, Object... args) {
         if (!Level.OFF.name().equals(LogTransaction.getLevel())) {
-            consumer.accept(LoggerFactory.getLogger(jointPoint.getTarget().getClass()));
+            LoggerFactory.getLogger(jointPoint.getTarget().getClass()).debug(msg, args);
         }
     }
 }
